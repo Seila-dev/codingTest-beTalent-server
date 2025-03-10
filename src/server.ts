a81ad71c64d6a38f998bfcb6c0ce0432d197fc50
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-03-10 18:40:39 -0300

    fix: fix uploads path image

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import { app } from './app'
 import employeeRoutes from './routes/employees-routes'
 import rolesRoutes from './routes/roles-routes'
+import path from 'path'
+import express from 'express'
 
 const port = 3000
+
+const publicPath = path.join(process.cwd(), 'uploads');
+app.use('/uploads', express.static(publicPath));
 
 app.listen(port, () => {
     console.log(`HTTP Server Running! Server: http://localhost:${port}`)
