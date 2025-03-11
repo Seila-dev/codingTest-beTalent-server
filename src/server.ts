d1de51c34b55090ea7264a14579ac2862535c25e
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-03-11 10:41:12 -0300

    docs: create swagger json

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import employeeRoutes from './routes/employees-routes'
 import rolesRoutes from './routes/roles-routes'
 import path from 'path'
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 const port = 3000
 
@@ -13,6 +15,6 @@ app.listen(port, () => {
     console.log(`HTTP Server Running! Server: http://localhost:${port}`)
 })
 
-// app uses
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/employees', employeeRoutes)
 app.use('/roles', rolesRoutes)
